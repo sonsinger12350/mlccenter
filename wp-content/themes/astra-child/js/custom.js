f9493b7168f29ed1844111b722b4aa950accb106
Son Le <sonsinger.sl@gmail.com>
2025-05-30 21:44:25 +0700

    [Page] Update front page

--- wp-content/themes/astra-child/js/custom.js
+++ wp-content/themes/astra-child/js/custom.js
@@ -3,9 +3,17 @@ jQuery(function($) {
 		loop: false,
 		margin: 24,
 		nav: true,
-		dots: false,
+		autoplay: true,
+		autoplayTimeout: 5000,
+		autoplayHoverPause: true,
 		responsive: {
 			0: {
+				items: 1
+			},
+			768: {
+				items: 2
+			},
+			1199: {
 				items: 3
 			}
 		}
