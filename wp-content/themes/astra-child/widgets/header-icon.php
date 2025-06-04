ea162133a895826576737220d1c2bfa045a8f7df
Son Le <sonsinger.sl@gmail.com>
2025-06-04 23:27:46 +0700

    [Page] Update home page

--- wp-content/themes/astra-child/widgets/header-icon.php
+++ wp-content/themes/astra-child/widgets/header-icon.php
@@ -1,74 +1,56 @@
 <?php
+
 use Elementor\Icons_Manager;
 
-class Astra_Child_Custom_Widget_Header_Icon extends WP_Widget {
-    public function __construct() {
-        parent::__construct(
-            'astra_child_custom_widget_header_icon',
-            __('Header Information', 'astra-child'),
-            array('description' => __('Header Information', 'astra-child'))
-        );
-    }
+class Astra_Child_Custom_Widget_Header_Icon extends WP_Widget
+{
+	public function __construct()
+	{
+		parent::__construct(
+			'astra_child_custom_widget_header_icon',
+			__('Header Information', 'astra-child'),
+			array('description' => __('Header Information', 'astra-child'))
+		);
+	}
 
-    public function form($instance) {
-        ?>
-        <p>
-          <label for="<?php echo esc_attr($this->get_field_id('address')); ?>"><?php _e('Địa chỉ:', 'astra-child'); ?></label>
-          <input class="widefat" id="<?php echo esc_attr($this->get_field_id('address')); ?>"
-                 name="<?php echo esc_attr($this->get_field_name('address')); ?>" type="text"
-                 value="<?php echo esc_attr($instance['address']); ?>">
-        </p>
-        <p>
-          <label for="<?php echo esc_attr($this->get_field_id('working_time')); ?>"><?php _e('Thời gian làm việc:', 'astra-child'); ?></label>
-          <input class="widefat" id="<?php echo esc_attr($this->get_field_id('working_time')); ?>"
-                 name="<?php echo esc_attr($this->get_field_name('working_time')); ?>" type="text"
-                 value="<?php echo esc_attr($instance['working_time']); ?>">
-        </p>
-        <p>
-          <label for="<?php echo esc_attr($this->get_field_id('hotline')); ?>"><?php _e('Hotline:', 'astra-child'); ?></label>
-          <input class="widefat" id="<?php echo esc_attr($this->get_field_id('hotline')); ?>"
-                 name="<?php echo esc_attr($this->get_field_name('hotline')); ?>" type="text"
-                 value="<?php echo esc_attr($instance['hotline']); ?>">
-        </p>
-        <?php
-    }
+	public function form($instance)
+	{
+		?>
+				<p>
+					<label for="<?php echo esc_attr($this->get_field_id('name')); ?>"><?php _e('Tên cơ sở:', 'astra-child'); ?></label>
+					<input class="widefat" id="<?php echo esc_attr($this->get_field_id('name')); ?>"
+						name="<?php echo esc_attr($this->get_field_name('name')); ?>" type="text"
+						value="<?php echo esc_attr($instance['name']); ?>">
+				</p>
+				<p>
+					<label for="<?php echo esc_attr($this->get_field_id('phone')); ?>"><?php _e('Số điện thoại:', 'astra-child'); ?></label>
+					<input class="widefat" id="<?php echo esc_attr($this->get_field_id('phone')); ?>"
+						name="<?php echo esc_attr($this->get_field_name('phone')); ?>" type="text"
+						value="<?php echo esc_attr($instance['phone']); ?>">
+				</p>
+		<?php
+	}
 
-    public function update($new_instance, $old_instance) {
-        return [
-			'address' => !empty($new_instance['address']) ? sanitize_text_field($new_instance['address']) : '',
-			'working_time' => !empty($new_instance['working_time']) ? sanitize_text_field($new_instance['working_time']) : '',
-			'hotline' => !empty($new_instance['hotline']) ? sanitize_text_field($new_instance['hotline']) : '',
+	public function update($new_instance, $old_instance)
+	{
+		return [
+			'name' => !empty($new_instance['name']) ? sanitize_text_field($new_instance['name']) : '',
+			'phone' => !empty($new_instance['phone']) ? sanitize_text_field($new_instance['phone']) : '',
 		];
-    }
+	}
 
-    public function widget($args, $instance) {
+	public function widget($args, $instance)
+	{
 		echo $args['before_widget'];
 
 		// Hiển thị địa chỉ với icon bản đồ
-		if (!empty($instance['address'])) {
+		if (!empty($instance['name'])) {
 			echo '<div class="header-icon-widget">';
-			Icons_Manager::render_icon( ['value' => 'fas fa-map-marked-alt', 'library' => 'fa-solid'], [ 'aria-hidden' => 'true' ] );
-			echo '<a href="https://www.google.com/maps/search/' . esc_html($instance['address']) . '" target="_blank">' . esc_html($instance['address']) . '</a>';
+			echo '<img class="search-icon" src="' . get_stylesheet_directory_uri() . '/assets/icon/icon-dienthoai.png" alt="icon-dienthoai">';
+			echo '<span>' . esc_html($instance['name']) . ':</span> <a href="tel:' . esc_html($instance['phone']) . '">' . esc_html($instance['phone']) . '</a>';
 			echo '</div>';
 		}
-	
-		// Hiển thị thời gian làm việc với icon đồng hồ
-		if (!empty($instance['working_time'])) {
-			echo '<div class="header-icon-widget">';
-			Icons_Manager::render_icon( ['value' => 'far fa-clock', 'library' => 'fa-solid'], [ 'aria-hidden' => 'true' ] );
-			echo '<span>' . esc_html($instance['working_time']) . '</span>';
-			echo '</div>';
-		}
-	
-		// Hiển thị hotline với icon điện thoại
-		if (!empty($instance['hotline'])) {
-			echo '<div class="header-icon-widget">';
-			Icons_Manager::render_icon( ['value' => 'fas fa-phone-alt', 'library' => 'fa-solid'], [ 'aria-hidden' => 'true' ] );
-			echo '<a href="tel:' . esc_html($instance['hotline']) . '">' . esc_html($instance['hotline']) . '</a>';
-			echo '</div>';
-		}
-	
+
 		echo $args['after_widget'];
 	}
-	
 }
