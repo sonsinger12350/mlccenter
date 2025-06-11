8afa2bae7c611277eeee4a523ff53efe211058c3
Son Le <sonsinger.sl@gmail.com>
2025-06-11 16:12:33 +0700

    [Plugin] Add Polylang

---
+++ wp-content/plugins/polylang/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wpsyntex/polylang',
+        'pretty_version' => '3.7.x-dev',
+        'version' => '3.7.9999999.9999999-dev',
+        'reference' => 'e7571e0f384b20aa3b6274c77c843e688192ec59',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'wpsyntex/polylang' => array(
+            'pretty_version' => '3.7.x-dev',
+            'version' => '3.7.9999999.9999999-dev',
+            'reference' => 'e7571e0f384b20aa3b6274c77c843e688192ec59',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
